56fc05857d2b9fac157c2eb2759b4d46e7d45ec1
HungAnhh <hungcao@gmx.de>
2022-09-13 09:20:25 +0200

    git push

---
+++ Chapter02_Variables/VariableNumbers2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+int main()
+{
+    // int = integer number
+    int my_number1;  // Declaration
+    my_number1 = 42; // Inizialisierung
+    int my_number2 = 1337;
+    int my_number3 = my_number2;
+
+    // unsigned int = can´t memory negativ
+    // numbers
+    unsigned int my_number4 = 0;
+
+    // float = floating point (32bit / 4byte)
+    float my_number5 = 1.25f;
+
+    // double = floating point (64bit / 8byte)
+    double my_number5 = 1.25;
+
+    return 0;
+}
