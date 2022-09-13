6e6972f9f1d813ec875686272803534b1033d4e2
HungAnhh <102995607+HungAnhh@users.noreply.github.com>
2022-09-13 09:48:25 +0200

    git commit

--- Chapter02_Variables/VariableCaculation.c
+++ Chapter02_Variables/VariableCaculation.c
@@ -3,6 +3,24 @@
 
 int main()
 {
+    float a = 5.0;
+    float b = 2.0;
+    float c;
+
+    // Addition
+    c = a + b;
+    printf("%f\n", c);
+    // Subtraction
+    c = a - b;
+    printf("%f\n", c);
+
+    // Multiplication
+    c = a * b;
+    printf("%f\n", c);
+
+    // Division
+    c = a / b;
+    printf("%f\n", c);
 
 
     return 0;
