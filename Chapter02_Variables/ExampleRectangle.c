ed300e6b8a732a13d66ce2ca394de2466e462ded
HungAnhh <102995607+HungAnhh@users.noreply.github.com>
2022-09-15 13:34:59 +0200

    git Commit

---
+++ Chapter02_Variables/ExampleRectangle.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+int main()
+{
+    // Rectangle Example
+    // 1.) Read the values for a, b from the user
+    // 2.) Compute the perimeter and area of the rectangle
+    // 3.) Print the computed values to the console
+
+    float a = 0.00f;
+    float b = 0.00f;
+
+    printf("Please enter the value for a: ");
+    scanf("%f", &a);
+    printf("\n");
+    printf("Please enter the value for b: ");
+    scanf("%f", &b);
+    printf("\n");
+
+    float perimeter = 2 * a + 2 * b;
+    float area = a * b;
+
+    printf("Perimeter: %f\n", perimeter);
+    printf("Area: %f\n", area);
+
+
+    return 0;
+}
