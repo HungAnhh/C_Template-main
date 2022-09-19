4761cffbf760c029acdbba3ca9cc7143337381fb
HungAnhh <102995607+HungAnhh@users.noreply.github.com>
2022-09-19 09:36:04 +0200

    git Commit

--- Chapter03_ControlFlow/Modulo.c
+++ Chapter03_ControlFlow/Modulo.c
@@ -3,6 +3,15 @@
 int main()
 {
 
+    int a = 9;
+    int b = 2;
+
+    int division = a / b;
+    int remainder = a % b;
+
+    printf("a / b = %f\n", division);
+    printf("a %% b = %d\n", remainder);
+
 
     return 0;
 }
